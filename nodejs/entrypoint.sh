fe247ebaf4da84898c10dace8874eb269e8abbbd
Yuri Serafim <yuri.serafim@okn.com.br>
2023-08-05 19:20:39 -0300

    feat: added nodejs docker image

---
+++ nodejs/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo chmod a+rw -R .
+
+echo ""
+echo ""
+echo "========================================"
+echo ""
+echo "CONTAINER STARTED SUCCESSFULLY!"
+echo ""
+echo "Press Ctrl + C to exit from container logs"
+echo ""
+echo "========================================"
+echo ""
+echo ""
+
+/bin/bash
